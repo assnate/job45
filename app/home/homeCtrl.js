72f1b02357bbbc2516f2c21e7a088c9703eac138
assnate <you@example.com>
2018-12-13 14:09:56 +0200

    comments

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,13 +1,27 @@
 
 app.controller("homeCtrl", function($scope, jobs) {
         debugger;
+        $scope.filteredJobs = [];
     // Checking if the user is currently logged in,
     // if not redirecting to the home page
+    $scope.searchText = "";
   
+    $scope.searchByName = function() {
+                debugger;
+                $scope.filteredJobs = [];
+        for(var i=0;i<$scope.jobs.length;i++) {
+            debugger;
+                if ($scope.jobs[i].name.toLowerCase().includes($scope.searchText.toLowerCase()))
+                {
+                    $scope.filteredJobs.push($scope.jobs[i]);
+                }
+        }
+    }
 
     jobs.getAllJobs().then(function (jobs) {
-        debugger;
+        $scope.filteredJobs = [];
         $scope.jobs = jobs;
+        $scope.filteredJobs = jobs;
     }, function(error) {
         
     });
